da7baa5c929c76c3b67a7812c84de92d6e74af92
diegora16 <diegoeureka982@gmail.com>
2024-07-29 19:36:42 -0500

    Actualizacion ver imagenes de personas BD

--- resources/views/personas.blade.php
+++ resources/views/personas.blade.php
@@ -4,44 +4,41 @@
 
 @section('content')
 
-<div class="status mb-5 mx-24 font-bold text-green-400 text-2xl">{{session('status')}}</div>
+<div class="status mb-5 mx-24 font-bold text-green-400 text-2xl">{{ session('status') }}</div>
 
-<section class="md:flex md:justify-around">
+<section class="md:flex md:flex-wrap md:justify-center gap-8">
 
+    <h1 class="font-bold text-2xl mb-4 mt-24">PERSONAS DISPONIBLES</h1>
 
-
-<div>
-<h1 class="font-bold">PERSONAS DISPONIBLES</h2>
-
-
-
-
-@foreach ($personas as $item )
-
-    <div class="md: flex">
-         <li class="mx-2">{{ $item->cPerNombre }} {{ $item->cPerApellido}}</li> 
-         <a class="text-sky-300" href="{{route('posts.edit',$item)}}">Editar</a>
-         <form action="{{ route('posts.destroy', $item) }}" method="POST" enctype="multipart/formdata">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="mx-2 text-red-500">Eliminar</button>
-         </form>
+    @foreach ($personas as $item)
+    <div class="card bg-purple-900 text-white rounded-2xl shadow-md overflow-hidden w-60 p-4">
+        <div class="card-image">
+            @if($item->image)
+            <img src="{{ asset('storage/' . $item->image) }}" alt="Imagen de {{ $item->cPerNombre }}" class="w-full h-40 object-cover rounded-t-2xl">
+            @else
+            <img src="default_image_path" alt="Sin imagen" class="w-full h-40 object-cover rounded-t-2xl">
+            @endif
+        </div>
+        <div class="card-content mt-4">
+            <h3 class="font-bold text-lg mb-2 text-center">{{ $item->cPerNombre }} {{ $item->cPerApellido }}</h3>
+            <div class="flex justify-between mt-4">
+                <a href="{{ route('posts.edit', $item) }}" class="text-blue-300 hover:underline">Editar</a>
+                <form action="{{ route('posts.destroy', $item) }}" method="POST" class="inline">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="text-red-300 hover:underline">Eliminar</button>
+                </form>
+            </div>
+        </div>
     </div>
-
-    
-    
-@endforeach
-
-</div>
-    
-<div>
-<a href="{{ route('posts.create') }}">
-<button class="font-bold bg-blue-500 hover:bg-blue-700 rounded-full px-4 py-1">Agregar Persona</button>
-</a>
-
-
-</div>
+    @endforeach
 
 </section>
-@endsection
 
+<div class="mt-8 flex justify-center">
+    <a href="{{ route('posts.create') }}">
+        <button class="font-bold bg-blue-500 hover:bg-blue-700 rounded-full px-4 py-1">Agregar Persona</button>
+    </a>
+</div>
+
+@endsection
